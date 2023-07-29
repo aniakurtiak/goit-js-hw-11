3736f6a6a1f0b356e8945fbfdcd4636ebffb3283
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-30 01:38:31 +0300

    request

---
+++ src/pixabay-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+ export function serviceSearchImg(input) {
+
+    const API_KEY = "38534343-83a5af4ee16ad6e7691f4452e";
+    const BASE_URL = 'https://pixabay.com/api/';
+
+    const searchParams = new URLSearchParams({
+        key: API_KEY,
+        q: input,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true
+    
+    });
+     
+     return axios.get(`${BASE_URL}?${searchParams}`)
+     .then(response => response.data)
+        .catch(error => {
+            console.log("Sorry, there are no images matching your search query. Please try again.", error);
+            return [];
+        });
+}
+
--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
-import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { serviceSearchImg } from "./pixabay-api";
+
+const form = document.querySelector('.search-form');
+
+
+form.addEventListener('submit', handlerSearch)
+
+function handlerSearch(evt) {
+    evt.preventDefault()
+    const { searchQuery } = evt.currentTarget.elements;
+    console.log(searchQuery);
+
+    serviceSearchImg(searchQuery.value)
+}
 
