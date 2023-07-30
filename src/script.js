2374c89a815a6a04fc8cb77ca23218d7710bf11c
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-30 03:01:11 +0300

    markUp

--- src/script.js
+++ src/script.js
@@ -2,18 +2,36 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { serviceSearchImg } from "./pixabay-api";
 
 const form = document.querySelector('.search-form');
-
+const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', handlerSearch)
 
 function handlerSearch(evt) {
     evt.preventDefault()
     const { searchQuery } = evt.currentTarget.elements;
-    console.log(searchQuery);
 
-    serviceSearchImg(searchQuery.value);
+    serviceSearchImg(searchQuery.value)
+        .then(data => gallery.innerHTML = createMarkup(data.hits))
+        .catch(error => console.log(error))
 }
 
 function createMarkup(arr) {
-    return arr.map().join('')
+    return arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `
+    <div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads${downloads}</b>
+    </p>
+  </div>
+</div>`).join('')
 }
